c6fc48bba1267edd25004916e6efa80f90a4a5e2
steve0503 <leehr76@gmail.com>
2014-01-20 17:43:21 +0900

    APNTest_2_3_6

--- APNTest/AppDelegate.m
+++ APNTest/AppDelegate.m
@@ -10,12 +10,8 @@
 
 @implementation AppDelegate
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
-{
-    // Override point for customization after application launch.
-    return YES;
-}
-							
+@synthesize window = _window;
+
 - (void)applicationWillResignActive:(UIApplication *)application
 {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
@@ -24,7 +20,7 @@
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
 
@@ -42,5 +38,29 @@
 {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
+
+-(void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken{
+    
+    NSLog(@"Sucess to register APN:%@",deviceToken);
+    
+    
+}
+
+-(void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error{
+    
+    NSLog(@"Fail to register APN: %@",[error localizedDescription]);
+    
+}
+
+-(BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions{
+    
+    UIRemoteNotificationType notiType = UIRemoteNotificationTypeBadge |
+    UIRemoteNotificationTypeAlert | UIRemoteNotificationTypeSound;
+    
+    [[UIApplication sharedApplication] registerForRemoteNotificationTypes:notiType];
+    
+    return YES;
+}
+
 
 @end
